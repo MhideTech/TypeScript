f3e138dcb7896086ed64e7996bfb77e6f66fbf5f
MhideTech <okefolahanolamide2006@gmail.com>
2024-02-27 20:53:21 -0800

    Learnt about some other datatypes

--- Lesson 1/build/js/index.js
+++ Lesson 1/build/js/index.js
@@ -7,3 +7,45 @@ let aboutMe = "🤷‍♀️";
 const sum = (a, b) => {
     return a + b;
 };
+// implicit
+let stringArr = ["ade", "bola", "chale"]; // str arr
+let guitar = ["Strat", "Les Paul", 5150]; // union arr (string or integer)
+let anyArr = []; // any arr
+// explicit
+let test = ["ade", "bola", "shade"];
+// tuple
+let myTuple = ["Orlah", 20, true];
+// Objects
+let myObj = {
+    prop1: "Orlah",
+    prop2: 32,
+    prop3: true,
+};
+console.log(myObj);
+let evh = {
+    name: "Orlah",
+    active: true,
+    albums: [1934, 3432, "The Glory Experience"],
+};
+let person1 = {
+    name: "Dammy",
+    noOfEyes: 2,
+    canWalk: true,
+};
+const greetPerson = (person) => {
+    if (person.name)
+        return `Hello ${person.name.toUpperCase()}`;
+    return `Hello!`;
+};
+console.log(greetPerson(person1));
+// Enums
+var Grade;
+(function (Grade) {
+    Grade[Grade["F"] = 0] = "F";
+    Grade[Grade["E"] = 1] = "E";
+    Grade[Grade["D"] = 2] = "D";
+    Grade[Grade["C"] = 3] = "C";
+    Grade[Grade["B"] = 4] = "B";
+    Grade[Grade["A"] = 5] = "A";
+})(Grade || (Grade = {})); // element @ first postion = 0
+console.log(Grade.F);
--- Lesson 1/src/index.ts
+++ Lesson 1/src/index.ts
@@ -7,3 +7,65 @@ let aboutMe: any = "🤷‍♀️";
 const sum = (a: number, b: number) => {
   return a + b;
 };
+
+// implicit
+let stringArr = ["ade", "bola", "chale"]; // str arr
+let guitar = ["Strat", "Les Paul", 5150]; // union arr (string or integer)
+let anyArr = []; // any arr
+
+// explicit
+let test: string[] = ["ade", "bola", "shade"];
+
+// tuple
+let myTuple: [string, number, boolean] = ["Orlah", 20, true];
+
+// Objects
+let myObj: object = {
+  prop1: "Orlah",
+  prop2: 32,
+  prop3: true,
+};
+console.log(myObj);
+
+type Guitarist = {
+  name: string;
+  active: boolean;
+  albums: (string | number)[];
+};
+
+let evh: Guitarist = {
+  name: "Orlah",
+  active: true,
+  albums: [1934, 3432, "The Glory Experience"],
+};
+
+// Interface
+interface Person {
+  name?: string;
+  noOfEyes: number;
+  canWalk: boolean;
+}
+
+let person1: Person = {
+  name: "Dammy",
+  noOfEyes: 2,
+  canWalk: true,
+};
+
+const greetPerson = (person: Person) => {
+  if (person.name) return `Hello ${person.name.toUpperCase()}`;
+  return `Hello!`;
+};
+
+console.log(greetPerson(person1));
+
+// Enums
+enum Grade {
+  F,
+  E,
+  D,
+  C,
+  B,
+  A,
+} // element @ first postion = 0
+console.log(Grade.F);
